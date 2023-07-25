31e8093a3ed2e96fda3f01916892405a4bab1f0f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-25 20:56:01 +0900

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+const $speakText = document.getElementById("speak-text");
+const speakText = async text => {
+  const url = `https://synthesis-service.scratch.mit.edu/synth?locale=JA-JP&gender=female&text=${text}`;
+
+  const dataUrl = URL.createObjectURL(await fetch(url).then(res => res.blob()));
+
+  const sound = new Howl({
+    src: [dataUrl],
+    html5: true,
+    onend: () => { sound.unload(); },
+  });
+
+  sound.play();
+};
+$speakText.addEventListener("keydown", (evt) => {
+  if (evt.key.toLowerCase() === "enter") {
+    speakText($speakText.value);
+    $speakText.value = ""
+  }
+});
+
+const data = {
+  consonantList: [],
+};
+for (const vowel of document.getElementsByClassName("vowels")) {
+  vowel.addEventListener("pointerover", (evt) => {
+    evt.target.classList.add("bg-gray-300");
+    const targetText = romajiConv(data.consonantList.join("") + evt.target.textContent).toHiragana();
+    data.consonantList = [];
+    speakText(targetText);
+    evt.preventDefault();
+  });
+  vowel.addEventListener("pointerleave", (evt) => {
+    evt.target.classList.remove("bg-gray-300");
+    evt.preventDefault();
+  });
+}
+for (const consonant of document.getElementsByClassName("consonant")) {
+  consonant.addEventListener("click", (evt) => {
+    data.consonantList.push(evt.target.textContent)
+  });
+}
